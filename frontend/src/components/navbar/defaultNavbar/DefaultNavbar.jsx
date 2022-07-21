cf9b00408fc7d329ec4d281693f5278e48e251cd
deeplahane <deeplahane15@gmail.com>
2022-07-22 00:04:13 +0530

    navbar changes

--- frontend/src/components/navbar/defaultNavbar/DefaultNavbar.jsx
+++ frontend/src/components/navbar/defaultNavbar/DefaultNavbar.jsx
@@ -1,27 +1,32 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import SearchBox from '../../searchbox/SearchBox';
-import './DefaultNavbar.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import SearchBox from "../../searchbox/SearchBox";
+import "./DefaultNavbar.css";
 
 const DefaultNavbar = () => {
-    return (
-        <nav className='navbar'>
-            <div className='left_side'>
-                <img src='/Images/logo/logo_icon.png' alt='' height={36} width={36} />
-                <span>EduCrat</span>
-            </div>
-            <div className='mid' >
-                <SearchBox/>
-            </div>
-            <div className='right_side'>
-                <Link to={"/signup-tutor"}>Become a Tutor</Link>
-                <Link to={"/login"}>Sign in</Link>
-                <Link to={"/signup"}>
-                    <button>Join</button>
-                </Link>
-            </div>
-        </nav>
-    )
-}
+  return (
+    <nav className="navbar">
+      <div className="left_side">
+        <img src="/Images/logo/logo_icon.png" alt="" height={36} width={36} />
+        <span className="name">EduCrat</span>
+      </div>
+      <div className="mid">
+        <SearchBox />
+      </div>
+      <div className="right_side">
+        <Link to={"/signup-tutor"}>
+          <span>Become a Tutor</span>
+        </Link>
 
-export default DefaultNavbar
+        <Link to={"/login"}>
+          <span>Sign in</span>
+        </Link>
+        <Link to={"/signup"}>
+          <button >Join</button>
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default DefaultNavbar;
